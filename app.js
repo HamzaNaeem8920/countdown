47e458c58e298b02737b9f123ba9ec14cde7d9ff
HamzaNaeem8920 <hnaeem8920@gmail.com>
2023-04-04 09:43:49 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const endDate = "24 April 2023 00:00 AM";
+
+document.getElementById("end-Date").innerText= `We are launching on ${endDate}`;
+
+const inputs= document.querySelectorAll("input");
+
+function clock() { 
+    const end= new Date(endDate)
+    const now= new Date()
+    console.log(end,now)
+    const diff = (end-now)/1000;
+    console.log(diff);
+    inputs[0].value =Math.floor(diff/3600/24);
+    inputs[1].value =Math.floor((diff/3600)%24);
+    inputs[2].value =Math.floor((diff/60)%24);
+    inputs[3].value =Math.floor(diff%60);
+
+
+}
+
+clock()
+
+setInterval(() => {
+    clock()
+}, 1000);
